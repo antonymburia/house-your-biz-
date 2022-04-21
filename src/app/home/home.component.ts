3217919808f019a5ddc6ae160526333c4aca6e46
antonymburia <antonymburia10@gmail.com>
2022-04-21 22:13:03 +0300

    create an api krequest

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,10 +9,21 @@ import { Api } from '../quote-class/api';
 })
 export class HomeComponent implements OnInit {
 
-  
-  constructor() { }
+  stall!: Api
+  constructor(public http: HttpClient) { }
 
   ngOnInit(): void {
+    
+    interface ApiResponse {
+      housename: string;
+      description: string;
+      location: string;
+    }
+    
+    this.http.get<ApiResponse>("http://127.0.0.1:8000/articles").subscribe(data => {
+      // Succesful API request
+      this.stall = new Api(data.housename, data.description,data.location)
+    })
   }
 
 }
